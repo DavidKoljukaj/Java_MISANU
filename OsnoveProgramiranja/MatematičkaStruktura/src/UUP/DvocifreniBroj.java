87d6ffbe184922ec9aba8d4c7d13d8f15963422a
DavidKoljukaj <131187601+DavidKoljukaj@users.noreply.github.com>
2023-04-23 04:24:10 +0200

    Add files via upload

---
+++ OsnoveProgramiranja/MatematičkaStruktura/src/UUP/DvocifreniBroj.java
@@ -0,0 +1,21 @@
+package UUP; 
+import java.util.Scanner; 
+public class DvocifreniBroj { 
+	public static void main(String[] args) { 
+		Scanner input = new Scanner(System.in); 
+		
+		System.out.println("Cifre dvocifrenog broja "); 
+		System.out.println("Dvocifreni broj "); 
+		int a = input.nextInt(); 
+		
+		int d, j; 
+		d = a / 10; 
+		j = a % 10; 
+		
+		System.out.println("broj = " + a + " " + "cifra jedinica = " + j +" " + "cifra desetica = " + d); 
+		// razlika izmedu cifara
+		int r = d - j;
+		System.out.println("Razlika cifara je : " + r);
+	} 
+	
+}
---
+++ OsnoveProgramiranja/MatematičkaStruktura/src/UUP/Promenljiva.java
@@ -0,0 +1,13 @@
+package UUP;
+
+public class Promenljiva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x,y,z;
+		x = y = z = 150;
+		System.out.println(x+y+z);
+
+	}
+
+}
