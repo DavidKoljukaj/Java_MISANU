9e3924323cd4f2352df576e55677117f9af56abf
DavidKoljukaj <131187601+DavidKoljukaj@users.noreply.github.com>
2023-04-23 04:25:36 +0200

    Add files via upload

---
+++ OsnoveProgramiranja/MetodeII/src/david/koljukaj/Primer14.java
@@ -0,0 +1,15 @@
+package david.koljukaj;
+
+public class Primer14 {
+	// Metoda za prevođenje Celzijusovih u Farenthajtove stepene
+	public static double CuF(double celsius) {
+		return (((0.9 / 0.5) * celsius) + 32);
+	}
+
+	public static void main(String[] args) throws Exception {
+		// Izmerena temperatura
+		double temp = 25;
+		// Štampanje rezultata
+		System.out.println("Temperatura prevedena u Farenthajte iznosi: " + CuF(temp));
+	}
+}
